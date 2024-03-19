ddd4dc79e42d31db62ab46d56ca35985c3c36d9a
Elena <kosnitskaya@yandex.ru>
2024-03-19 21:05:14 +0300

    Make my first test

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void firstTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
